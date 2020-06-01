3ee83f2c53cdba063725e5301e13673ac1bec57d
chinguyen98 <dacchi6761@gmail.com>
2020-06-01 19:04:45 +0700

    api

---
+++ app/Http/Controllers/Api/CoffeeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CoffeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $danhsachcoffee = DB::table('coffees')->orderByDesc('id')->get();
+        return response()->json($danhsachcoffee);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        DB::table('coffees')->insert([
+            'name' => $request->name,
+            'price' => $request->price,
+            'image' => 'legendsuc8.jpg',
+            'capacity' => 250,
+            'info' => 'test',
+            'specific' => 'CHua ro',
+            'ingredient' => 'Chua ro',
+            'expired' => 24,
+            "id_brand" => 1,
+            "id_type" => 5,
+            "id_unit" => 1
+        ]);
+        $danhsachcoffee = DB::table('coffees')->orderByDesc('id')->get();
+        return response()->json($danhsachcoffee);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $coffee = DB::table('coffees')->where('id', $id)->first();
+        return response()->json($coffee);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        DB::table('coffees')->where('id', $id)->update(['name' => $request->name, 'price' => $request->price]);
+        $danhsachcoffee = DB::table('coffees')->orderByDesc('id')->get();
+        return response()->json($danhsachcoffee);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table('coffees')->delete($id);
+        $danhsachcoffee = DB::table('coffees')->orderByDesc('id')->get();
+        return response()->json($danhsachcoffee);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//REST API
+//GET: /coffees -> Lay ve het coffee -> function index()
+//GET: /coffees/{id} -> Lay ve 1 coffee -> function show()
+//POST: /coffees -> Tao 1 coffee -> function store()
+//PUT: /coffees/{id} -> Cap nhat 1 coffee -> function update()
+//DELETE: /coffees/{id} -> Xoa 1 coffee -> function destroy()
+
+Route::apiResource('coffees', 'Api\CoffeeController');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
